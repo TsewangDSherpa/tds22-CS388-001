83be67c6f201548565f1b7d5541122360b48c701
Tsewanghyolmo01 <tds22@njit.edu>
2024-09-19 11:19:37 -0400

    Lab 2

---
+++ Labs/Lab2/app/src/main/java/com/sherpa/wmail/Email.kt
@@ -0,0 +1,8 @@
+package com.sherpa.wmail
+
+class Email (
+    val sender: String,
+    val title: String,
+    val summary: String ) {
+
+}
---
+++ Labs/Lab2/app/src/main/java/com/sherpa/wmail/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.sherpa.wmail
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var emails: MutableList<Email>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Lookup the RecyclerView in activity layout
+        val emailsRv = findViewById<RecyclerView>(R.id.emailsRv)
+        // Fetch the list of emails
+        emails = EmailFetcher.getEmails()
+        // Create adapter passing in the list of emails
+        val adapter = EmailAdapter(emails)
+        // Attach the adapter to the RecyclerView to populate items
+        emailsRv.adapter = adapter
+        // Set layout manager to position the items
+        emailsRv.layoutManager = LinearLayoutManager(this)
+
+        findViewById<Button>(R.id.loadMoreBtn).setOnClickListener {
+            val newEmails = EmailFetcher.getNext5Emails()
+            emails.addAll(newEmails)
+            // Notify the adapter there's new emails so the RecyclerView layout is updated
+            adapter.notifyDagit statusgit statustaSetChanged()
+        }
+    }
+}

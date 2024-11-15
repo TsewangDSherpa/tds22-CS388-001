b0d5747d5d26ea95d78ed66d1adf02a094fad1a1
Tsewanghyolmo01 <tds22@njit.edu>
2024-11-14 22:33:45 -0500

    Finished Lab6

---
+++ Labs/Lab6Final/app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.codepath.articlesearch
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.codepath.articlesearch.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.serialization.json.Json
+
+fun createJson() = Json {
+    isLenient = true
+    ignoreUnknownKeys = true
+    useAlternativeNames = false
+}
+
+private const val TAG = "MainActivity/"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        // Define your fragments
+        val bestSellerBooksFragment: Fragment = BestSellerBooksFragment()
+        val articleListFragment: Fragment = ArticleListFragment()
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // Handle navigation selection
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            val fragment: Fragment = when (item.itemId) {
+                R.id.nav_books -> bestSellerBooksFragment
+                R.id.nav_articles -> articleListFragment
+                else -> bestSellerBooksFragment
+            }
+            replaceFragment(fragment)
+            true
+        }
+
+        // Set default selection
+        bottomNavigationView.selectedItemId = R.id.nav_books
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.article_frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+}
